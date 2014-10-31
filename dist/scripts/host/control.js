8e2c9ecad42b90d492e8dcddd4e7c5d17a524a37
Isaiah Miller <I.gosaiah@gmail.com>
2014-10-31 03:23:08 -0400

    Revert 7bcbac7..0cc5206
    This rolls back to commit 7bcbac70e88c8850fc949a6d1513151fbe355c2a.

--- dist/scripts/host/control.js
+++ dist/scripts/host/control.js
@@ -26,18 +26,8 @@ var TSOS;
             // Get a global reference to the canvas.  TODO: Move this stuff into a Display Device Driver, maybe?
             _Canvas = document.getElementById('display');
 
-            var statusBar = document.getElementById('taStatusBarStatus');
-            statusBar.innerHTML = "On";
-
             // Get a global reference to the drawing context.
             _DrawingContext = _Canvas.getContext('2d');
-
-            // Get a global Reference to the status bar
-            _StatusCanvas = document.getElementById("statusCanvas");
-            _StatusCanvas = _StatusCanvas.getContext('2d');
-
-            // Enable the text functions
-            TSOS.CanvasTextFunctions.enable(_StatusCanvas);
 
             // Enable the added-in canvas text functions (see canvastext.ts for provenance and details).
             TSOS.CanvasTextFunctions.enable(_DrawingContext); // Text functionality is now built in to the HTML5 canvas. But this is old-school, and fun.
@@ -71,10 +61,6 @@ var TSOS;
             // Update the log console.
             var taLog = document.getElementById("taHostLog");
             taLog.value = str + taLog.value;
-
-            var statusBar = document.getElementById("taStatusBarDate");
-            var date = new Date();
-            statusBar.innerHTML = date.toDateString() + " " + date.toLocaleTimeString();
             // Optionally update a log database or some streaming service.
         };
 
@@ -93,14 +79,14 @@ var TSOS;
             document.getElementById("display").focus();
 
             // ... Create and initialize the CPU (because it's part of the hardware)  ...
-            _CPU = new Cpu();
+            _CPU = new TSOS.Cpu();
             _CPU.init();
 
             // ... then set the host clock pulse ...
-            _hardwareClockID = setInterval(Devices.hostClockPulse, CPU_CLOCK_INTERVAL);
+            _hardwareClockID = setInterval(TSOS.Devices.hostClockPulse, CPU_CLOCK_INTERVAL);
 
             // .. and call the OS Kernel Bootstrap routine.
-            _Kernel = new Kernel();
+            _Kernel = new TSOS.Kernel();
             _Kernel.krnBootstrap();
         };
 
