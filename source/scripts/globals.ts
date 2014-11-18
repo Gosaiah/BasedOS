ada95845ccb7a2a86e1ea85779ea674db2942677
Isaiah Miller <I.gosaiah@gmail.com>
2014-11-18 12:15:54 -0500

    Revert 42dcf59..861d0f5
    This rolls back to commit 42dcf5951e43bc0a74a05a6b21471857bcbac321.

--- source/scripts/globals.ts
+++ source/scripts/globals.ts
@@ -13,7 +13,7 @@
 // TODO: Make a global object and use that instead of the "_" naming convention in the global namespace.
 //
 var APP_NAME: string    = "BasedOS";   // 'cause Bob and I were at a loss for a better name.
-var APP_VERSION: string = "11.17.1";   // Good way to keep track of dates
+var APP_VERSION: string = "11.02.2";   // Good way to keep track of dates
 
 var CPU_CLOCK_INTERVAL: number = 100;   // This is in ms, or milliseconds, so 1000 = 1 second.
 
